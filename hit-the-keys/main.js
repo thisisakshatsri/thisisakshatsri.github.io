5ae80f29ab58e0bde6799c8a92ac5a23ad331993
thisisakshatsri <akshat.srivastava643@gmail.com>
2022-01-30 14:57:58 +0530

    Bug Fixes

--- hit-the-keys/main.js
+++ hit-the-keys/main.js
@@ -45,79 +45,79 @@ function makeSound(key) {
             audio7.play();
             break;
         case "k":
-            var audio8 = document.querySelector("sounds/flash-3.mp3");
+            var audio8 = new Audio("sounds/flash-3.mp3");
             audio8.play();
             break;
         case "l":
-            var audio9 = document.querySelector("sounds/glimmer.mp3");
+            var audio9 = new Audio("sounds/glimmer.mp3");
             audio9.play();
             break;
         case "z":
-            var audio10 = document.querySelector("sounds/hihat.wav");
+            var audio10 = new Audio("sounds/hihat.wav");
             audio10.play();
             break;
         case "x":
-            var audio11 = document.querySelector("sounds/kick.wav");
+            var audio11 =new Audio("sounds/kick.wav");
             audio11.play();
             break;
         case "c":
-            var audio12 = document.querySelector("sounds/moon.mp3");
+            var audio12 = new Audio("sounds/moon.mp3");
             audio12.play();
             break;
         case "v":
-            var audio13 = document.querySelector("sounds/openhat.wav");
+            var audio13 = new Audio("sounds/openhat.wav");
             audio13.play();
             break;
         case "b":
-            var audio14 = document.querySelector("sounds/pinwheel.mp3");
+            var audio14 = new Audio("sounds/pinwheel.mp3");
             audio14.play();
             break;
         case "n":
-            var audio15 = document.querySelector("sounds/piston-1.mp3");
+            var audio15 = new Audio("sounds/piston-1.mp3");
             audio15.play();
             break;
         case "m":
-            var audio16 = document.querySelector("sounds/piston-2.mp3");
+            var audio16 =new Audio("sounds/piston-2.mp3");
             audio16.play();
             break;
         case "q":
-            var audio21 = document.querySelector("sounds/ride.wav");
+            var audio21 = new Audio("sounds/ride.wav");
             audio21.play();
             break;
         case "w":
-            var audio22 = document.querySelector("sounds/spiral.mp3");
+            var audio22 = new Audio("sounds/spiral.mp3");
             audio22.play();
             break;
         case "e":
-            var audio23 = document.querySelector("sounds/splits.mp3");
+            var audio23 = new Audio("sounds/splits.mp3");
             audio23.play();
             break;
         case "r":
-            var audio24 = document.querySelector("sounds/squiggle.mp3");
+            var audio24 =new Audio("sounds/squiggle.mp3");
             audio24.play();
             break;
         case "t":
-            var audio25 = document.querySelector("sounds/strike.mp3");
+            var audio25 = new Audio("sounds/strike.mp3");
             audio25.play();
             break;
         case "y":
-            var audio26 = document.querySelector('.sounds/suspension.mp3');
+            var audio26 =new Audio('.sounds/suspension.mp3');
             audio26.play();
             break;
         case "u":
-            var audio27 = document.querySelector("sounds/tom-1.mp3");
+            var audio27 =new Audio("sounds/tom-1.mp3");
             audio27.play();
             break;
         case "i":
-            var audio28 = document.querySelector("sounds/tom-2.mp3");
+            var audio28 = new Audio("sounds/tom-2.mp3");
             audio28.play();
             break;
         case "o":
-            var audio29 = document.querySelector("sounds/tom-3.mp3");
+            var audio29 = new Audio("sounds/tom-3.mp3");
             audio29.play();
             break;
         case "p":
-            var audio30 = document.querySelector("sounds/tom-4.mp3");
+            var audio30 = new Audio("sounds/tom-4.mp3");
             audio30.play();
             break;
         default: console.log(key);
@@ -130,5 +130,5 @@ function buttonAnimation(currentKey) {
     activeButton.classList.add("pressed");
     setTimeout(function () {
         activeButton.classList.remove("pressed");
-    }, 100);
+    }, 300);
 }
